1884d47d7304851663f620456a89774471940653
Robyn Kwok <jingwenkwok@gmail.com>
2017-10-25 21:07:46 -0400

    Init chatbot

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const dotenv = require('dotenv')
+
+// Configure .env path
+dotenv.load({path: '.env'})
+
+const app = express()
+
+app.set('port', (process.env.PORT || '5000'));
+
+// for POST requests
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// ROUTES
+
+app.get('/', (req, res) => {
+  res.send("Hello, world.")
+})
+
+// Connect to Facebook
+app.get('/webhook/', (req, res) => {
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode == 'subscribe' && token === process.env.VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Running port.');
+})
