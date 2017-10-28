7014c690dd31831820bc43d113918e883c496f93
Robyn Kwok <jingwenkwok@gmail.com>
2017-10-28 17:32:00 -0400

    hi

---
+++ server/db.js
@@ -0,0 +1,45 @@
+const firebase = require('./firebase');
+const db = firebase.database();
+
+module.exports = {
+  getUser: (user) => {
+    const userRef = db.ref('users/'+ user.id);
+    userRef.once("value", (snapshot) => {
+      if (snapshot.val() == null) {
+        userRef.set({
+          snackLimit: 0,
+          healthGoal: 0,
+          totalAverage: 0,
+          weeklyAverage: 0,
+          dailyAverage: 0
+          // meals: JSON, only implement when first meal added
+          // snacks: JSON, only implement when first snack added
+        })
+        return -1;
+      } else {
+        return 1;
+      }
+    })
+  },
+
+  setSnackLimit: (user, snackLimit) => {
+    const userRef = db.ref('users/' + user.id);
+    userRef.once("value", (snapshot) => {
+      userRef.set({
+        snackLimit: snackLimit
+      });
+    })
+  },
+
+  setHealthGoal: (user, healthGoal) => {
+    const userRef = db.ref('users/' + user.id);
+    userRef.once("value", (snapshot) => {
+      userRef.set({
+        healthGoal: healthGoal
+      });
+    })
+  }
+
+
+
+}
