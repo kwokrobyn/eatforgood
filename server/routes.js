615a77f9cf708772f47e0f765a451448aac5ad25
Robyn Kwok <jingwenkwok@gmail.com>
2017-11-09 14:29:33 -0500

    hi

--- server/routes.js
+++ server/routes.js
@@ -48,12 +48,14 @@ router.post('/webhook/', (req, res) => {
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
+
+    if event.postback {
+      bot.sendMessage(sender, {"text": "hello yooyo"});
+    }
+
     if (event.message && event.message.text) {
       let text = event.message.text;
       let payload = event.message.payload;
-      if event.postback {
-        bot.sendMessage(sender, {"text": "hello yooyo"});
-      }
 
       // // get referene for user from db
       // const userRef = db.ref('users/'+ sender);
